bea1c506f812239e961c9498c619e22252d27cab
Hampus Lundblad <hampus.lundblad@hotmail.se>
2024-12-06 18:05:52 +0100

    cleanup

--- evReactNative/App.tsx
+++ evReactNative/App.tsx
@@ -1,13 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 // Used for debugging network logs since react-native debugger doesn't support it
 import "./ReactotronConfig.js";
 
-import { onlineManager, QueryClient } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
-import NetInfo from "@react-native-community/netinfo";
 import RootNavigation from "./src/navigation/RootNavigation";
 
 const queryClient = new QueryClient({
